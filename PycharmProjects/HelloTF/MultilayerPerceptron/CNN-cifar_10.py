fe031ff98d02cab729628bd26aa0b6beb2025add
yangshuo <yangshuo@weidian.com>
2018-11-20 21:02:41 +0800

    贝叶斯

---
+++ PycharmProjects/HelloTF/MultilayerPerceptron/CNN-cifar_10.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env pytho      
+# -*- coding: utf-8 -*-
+# @Time    : 2018/11/19 4:09 PM
+# @Author  : YangShuo
+
+import cifar10, cifar10_input
--- PycharmProjects/HelloTF/MultilayerPerceptron/CNN-mnist.py
+++ PycharmProjects/HelloTF/MultilayerPerceptron/CNN-mnist.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env pytho      
+#!/usr/bin/env pytho
 # -*- coding: utf-8 -*-
 # @Time    : 2018/11/16 3:16 PM
 # @Author  : YangShuo
@@ -6,13 +6,12 @@
 from tensorflow.examples.tutorials.mnist import input_data
 import tensorflow as tf
 mnist = input_data.read_data_sets("MNIST_data/", one_hot=True)
-sess = tf.InteractiveSession
-
+sess = tf.InteractiveSession()
 '''
 设置初始化函数
 '''
 def weight_variable(shape):
-    initial = tf.truncated_normal(shape, stddev=0.1)  # 产生截断的标准差为0，1的正态分布的噪音
+    initial = tf.truncated_normal(shape, stddev=0.1)  # 产生截断的标准差为0.1的正态分布的噪音
     return tf.Variable(initial)
 
 def bias_variable(shape):
@@ -49,9 +48,9 @@ h_pool1 = max_pool_2x2(h_conv1)
 '''
 第二个卷积层，卷积核变为64 其他不变
 '''
-W_conv2 = weight_variable(([5, 5, 1, 64]))
+W_conv2 = weight_variable(([5, 5, 32, 64]))
 b_conv2 = bias_variable(([64]))
-h_conv2 = tf.nn.relu(conv2d(x_image, W_conv2) + b_conv2)
+h_conv2 = tf.nn.relu(conv2d(h_pool1, W_conv2) + b_conv2)
 h_pool2 = max_pool_2x2(h_conv2)
 
 '''经过两次2*2池化，尺寸变为四分之一，第二个卷积层的卷积核为64，所以想现在是 7*7*64'''
@@ -65,14 +64,14 @@ h_fc1 = tf.nn.relu(tf.matmul(h_pool2_flat, W_fc1) + b_fc1)
 dropout层 
 '''
 keep_prob = tf.placeholder(tf.float32)
-h_fc1_drop = tf.nn.dropout(h_fc1,keep_prob)
+h_fc1_drop = tf.nn.dropout(h_fc1, keep_prob)
 
 '''
 连接softmax层
 '''
-W_fc2 =weight_variable([1024, 10])
+W_fc2 = weight_variable([1024, 10])
 b_fc2 = bias_variable([10])
-y_conv=tf.nn.softmax(tf.matmul(h_fc1_drop, W_fc2) + b_fc2)
+y_conv = tf.nn.softmax(tf.matmul(h_fc1_drop, W_fc2) + b_fc2)
 
 '''
 定义损失函数 
@@ -81,13 +80,28 @@ cross_entropy = tf.reduce_mean(-tf.reduce_sum(y_ * tf.log(y_conv), reduction_ind
 train_step = tf.train.AdamOptimizer(1e-4).minimize(cross_entropy)
 
 '''
+评测准确率
+'''
+correct_prediction = tf.equal(tf.arg_max(y_conv, 1), tf.arg_max(y_, 1))
+accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
 
 '''
+训练过程
+'''
+sess.run(tf.global_variables_initializer())
 
 
 
+for i in range(20000):
+  batch = mnist.train.next_batch(50)
+  if i % 100 == 0:
+    train_accuracy = accuracy.eval(feed_dict={
+        x: batch[0], y_: batch[1], keep_prob: 1.0})
+    print("step %d, training accuracy %g" % (i, train_accuracy))
+  train_step.run(feed_dict={x: batch[0], y_: batch[1], keep_prob: 0.5})
 
-
+print("test accuracy %g" % accuracy.eval(feed_dict={
+    x: mnist.test.images, y_: mnist.test.labels, keep_prob: 1.0}))
 
 
 
