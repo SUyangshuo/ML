fd8de49d155356d3291d7e7aeef9d5daeccfe3bb
yangshuo <yangshuo@weidian.com>
2018-12-18 21:09:39 +0800

    word2vec

--- PycharmProjects/HelloTF/word2vec/rnn-word2vec.py
+++ PycharmProjects/HelloTF/word2vec/rnn-word2vec.py
@@ -46,3 +46,36 @@ print('Data size', len(word))
 '''
 创建vocabulary词汇表
 '''
+## 保存top50000的词汇
+vocabulary_size = 50000
+
+def build_dataset(words):
+    count = [['UNK', -1]]
+    # collections.Counter取出单词列表中的单词频数，most_common取top50000中的作为一个词汇表
+    count.extend(collections.Counter(words).most_common(vocabulary_size - 1))
+    dictionary = dict()  # 创建一个空字典
+
+    for word, _ in count:
+        dictionary[word] = len(dictionary)  # 查看该词是否在字典表中
+    data = list()  # 元组(元组里的数据不能修改 元组是括号) 转化为列表(列表是中括号 元素可以修改)
+    unk_count = 0
+    for word in words:
+        if word in dictionary:
+            index = dictionary[word]
+        else:
+            index = 0
+            unk_count += 1
+        data.append(index)
+
+    count[0][1] = unk_count   #top50000以后的单词的数量
+    #  zip方法 是把两个迭代器一一对应 产出一个元组，再使用list方法可以转化为列表 dict是转化为一个字典，存储key和valueas
+    reverse_dictionary = dict(zip(dictionary.values(), dictionary.keys()))
+    return data, count, dictionary, reverse_dictionary
+data, count, dictionary, reverse_dictionary = build_dataset(words)
+
+
+
+
+
+
+
