59022a36c479491fead528598d6a7e212063ccb3
yangshuo <yangshuo@weidian.com>
2019-01-07 21:23:10 +0800

    ac

---
+++ IdeaProjects/src/exam/medium/palindromicNumber/PrimePalindrome.java
@@ -0,0 +1,13 @@
+package medium.palindromicNumber;
+
+/**
+ * @author YangShuo
+ * @create 2019/1/7
+ * @comment
+ */
+public class PrimePalindrome {
+    public int primePalindrome(int N) {
+
+        return 1;
+    }
+}
