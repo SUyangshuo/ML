0a774b315774776d9975cc727d01d8354db70cd3
yangshuo <yangshuo@weidian.com>
2019-01-15 21:38:25 +0800

    ac

--- IdeaProjects/src/exam/medium/dp/DecodeWaysFirst.java
+++ IdeaProjects/src/exam/medium/dp/DecodeWaysFirst.java
@@ -20,7 +20,7 @@ Given a non-empty string containing only digits, determine the total number of w
 public class DecodeWaysFirst {
     public static void main(String[] args) {
 
-        int i = numDecodings2("121");
+        int i = numDecodings2("24726");
         System.out.print("结果是" + i);
     }
 
@@ -122,14 +122,14 @@ public class DecodeWaysFirst {
                     dp[i] = dp[i - 1] + 1;
                 }else {
                     if(Integer.valueOf(lastTemp) >= 10 && Integer.valueOf(lastTemp) <= 26){
-                        dp[i] = dp[i - 2] + 2;
+                        dp[i] = dp[i - 2] +  dp[i - 1];
 
                     }else {
                         dp[i] = dp[i - 2] + 1;
                     }
 
                 }
-            }else {
+            }else {//中间有大于30 的
                 dp[i]=dp[i-1];
             }
             lastTemp=temp;
@@ -143,6 +143,62 @@ public class DecodeWaysFirst {
             }
 
             return dp[i];
+
+    }
+    //ac版本
+    public int numDecodings3(String s) {
+        int[] A=new int[s.length()];    //记录当前字符串第i个位置 的decode方法
+        if(s.length()<=1){
+            if(s.length()==0)
+                return 0;
+            else{
+                if(s.charAt(0)=='0')
+                    return 0;
+                else
+                    return 1;
+            }
+        }
+
+        if(s.length()>=2){
+            if(s.charAt(0)=='0')        //若包含0则返回0
+                return 0;
+        }
+
+        A[0]=1; //字符串第一位的数肯定只有一种匹配方法
+
+        int x=(s.charAt(0)-'0')*10+(s.charAt(1)-'0');       //前两位数的值
+        if(x>26 && x%10==0)
+            return 0;
+        if(x<=26&&x>0)
+            if(x!=10&&x!=20)
+                A[1]=2;
+            else {
+                A[1]=1;
+            }
+        else if(s.charAt(1)!='0')
+            A[1]=1;
+        else {
+            A[1]=0;
+        }
+
+        for(int i=2;i<s.length();i++){
+            //记录当前字符和前一个字符组成的数，若这个数小于等于26，则到当前的方法d(s.charAt(i))=d(s.charAt(i-1))+d(s.charAt(i-2)),否则d(s.charAt(i))=0
+            int temp=(s.charAt(i-1)-'0')*10+(s.charAt(i)-'0');
+            if(temp<=26&&temp>0)  //在0到26之间且前一个数不为0
+                if(s.charAt(i-1)!='0'&&s.charAt(i)!='0')
+                    A[i]=A[i-2]+A[i-1];
+                else A[i]=A[i-2];
+            else if(temp>26)     //中间有大于30的
+                if(temp%10!=0)
+                    A[i]=A[i-1];
+                else {
+                    return 0;
+                }
+            else        //包含两个连续的0
+                return 0;
+        }
+        return A[s.length()-1];
+
 
     }
 }
---
+++ IdeaProjects/src/exam/medium/ergodic/SurroundedRegions.java
@@ -0,0 +1,111 @@
+package medium.ergodic;
+
+/**
+ * @author YangShuo
+ * @create 2019/1/15
+ * @comment
+ */
+
+import java.util.LinkedList;
+
+/**
+ * Given a 2D board containing 'X' and 'O' (the letter O), capture all regions surrounded by 'X'.
+
+ A region is captured by flipping all 'O's into 'X's in that surrounded region.
+
+ Example:
+
+ X X X X
+ X O O X
+ X X O X
+ X O X X
+
+ After running your function, the board should be:
+
+ X X X X
+ X X X X
+ X X X X
+ X O X X
+
+ Explanation:
+
+ Surrounded regions shouldn’t be on the border, which means that any 'O' on the border of the board are not flipped to 'X'. Any 'O' that is not on the border and it is not connected to an 'O' on the border will be flipped to 'X'. Two cells are connected if they are adjacent cells connected horizontally or vertically.
+
+
+ 找到没有和边上的o连接的o  并把他们变成x
+
+ */
+
+//首先遍历周围一圈的字母，发现有o的就从该点进行BFS，并且把遍历到的o 改为$ 在周围一圈遍历完成后，把所有的$ 变成o 把所有的o变成x
+public class SurroundedRegions {
+
+    public void solve(char[][] board) {
+        if(board.length<=1 || board[0].length<=1){
+            return;
+        }
+        //确定矩阵的大小
+        int x= board.length;
+        int y= board[0].length;
+        for(int i=0;i<y;i++){
+           full(board,0,i); //第一行元素
+            full(board,x-1,i); //最后一行元素
+
+        }
+
+        for(int i=0;i<y;i++){
+            full(board,i,0); //第一列元素
+            full(board,i,y-1); //最后一列元素
+
+        }
+
+        //对于最终的矩阵进行转化
+
+        for(int i=0;i<x;i++){
+            for(int j=0;j<y;j++){
+
+            }
+        }
+    }
+    //利用bfs进行广度优先遍历，使用队列 先进先出
+    public void full(char[][] board,int x,int y){
+        if(board[x][y]=='x'){
+            return;
+        }else if(board[x][y]=='o'){
+            board[x][y]='x';
+        }
+        LinkedList<Integer> quen=new LinkedList<>();
+        //记录当前的坐标
+        Integer temp = x*board.length+y;
+        quen.add(temp);
+
+        while(!quen.isEmpty()){
+            int i=quen.poll()/board.length;
+            int z=quen.poll()%board.length;
+
+            //todo:查看该点上下左右的位置是否有‘o'
+            if(y-1>0 && board[y-1][x]=='o'){
+                quen.add(x*board.length+y-1);
+                board[x][y-1]='$';
+            }
+            if(x-1>0 && board[y][x-1]=='o'){
+                quen.add((x-1)*board.length+y);
+                board[x-1][y]='$';
+            }
+
+            if(y+1<board[0].length && board[y+1][x]=='o'){
+                quen.add(x*board.length+y+1);
+                board[x][y+1]='$';
+            }
+
+            if(x+1<board.length && board[x+1][y]=='o'){
+                quen.add((x+1)*board.length+y);
+                board[x+1][y]='$';
+            }
+        }
+
+
+
+
+
+    }
+}
