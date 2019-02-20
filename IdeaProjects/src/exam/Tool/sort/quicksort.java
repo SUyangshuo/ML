9b1850e8c0fa4dee9329a3dccba891d07fb5472f
yangshuo <yangshuo@weidian.com>
2019-02-20 21:38:49 +0800

    ac

--- IdeaProjects/src/exam/Tool/sort/quicksort.java
+++ IdeaProjects/src/exam/Tool/sort/quicksort.java
@@ -6,8 +6,11 @@ package Tool.sort;
  * @comment
  */
 public class quicksort {
-    //版本一：使用快速排序  使用第一个值为比较值
+    //版本一：使用快速排序  使用第一个值为比较值  时间复杂度为o(nlogn)
+
     public static void sortstep1(int[] a,int i,int j){
+
+        int k=0;//第k大的数字
         if(i>=j){
             return ;
         }
@@ -21,6 +24,20 @@ public class quicksort {
         }else {
             sortstep1(a,0,index-1);
         }
+
+        ////------------------------
+        int index2=sortstep2(a,i,j);
+
+        if(k<index2){
+           //说明k值在左边
+            sortstep1(a,0,index-1);
+        }else if(k>index2+1){
+            sortstep1(a,index+1,a.length-1);
+
+        }else if(k==index2+1){
+           //return a[index2];
+        }
+
 
 
     }
@@ -53,4 +70,32 @@ public class quicksort {
         a[y]=temp;
         return a;
     }
+
+
+    //快排变种：Quick Select  用在返回第k大的数字时候，时间复杂度可以降低为on
+    //改动点是在快排序遍历的时候，返回确定下标的时候和k进行比较
+
+    public static Integer sortstep1verxion2(int[] a,int i,int j,int k){
+
+        if(i>=j || k>a.length){
+            return null;
+        }
+        int temp=a.length-k;
+        int index2=sortstep2(a,i,j);
+
+
+        if(temp<index2){
+            //说明k值在左边
+            sortstep1verxion2(a,0,index2-1,k);
+        }else if(temp>index2){
+            sortstep1verxion2(a,index2+1,a.length-1,k);
+
+        }
+            return a[temp];
+
+
+
+
+    }
+
 }
--- IdeaProjects/src/exam/medium/sort/WiggleSortII.java
+++ IdeaProjects/src/exam/medium/sort/WiggleSortII.java
@@ -9,13 +9,41 @@ import java.util.Arrays;
  */
 public class WiggleSortII {
 
+//324. Wiggle Sort II
+    //传统方法 排序后 交叉排序,空间复杂度为o(n)
+
     public void wiggleSort(int[] nums) {
-        Arrays.sort(nums);
 
-        int length=nums.length;
+        int[] newNums = new int[nums.length];
+        int length = nums.length;
+        if(nums == null || length < 2)
+            return;
+        for(int i = 0; i < length; i++)
+            newNums[i] = nums[i];
 
-        if(length%2==0){//如果是偶数的话 平分数组
+        Arrays.sort(nums);  //排序
+        if(length == 2)
+            return;
+        int k = length - 1;
+        for(int i = 1; i < length; i = i + 2)
+        {
+            newNums[i] = nums[k];
+            k = k - 1;
+        }
+        for(int j = 0; j < length; j = j + 2)
+        {
+            newNums[j] = nums[k];
+            k = k - 1;
+        }
+        for(int i = 0; i < length; i++)
+        {
+            nums[i] = newNums[i];
+            //System.out.print(newNums[i] + "  ");
+        }
+
 
         }
-    }
+        //空间复杂度为o(1)
+
+
 }
