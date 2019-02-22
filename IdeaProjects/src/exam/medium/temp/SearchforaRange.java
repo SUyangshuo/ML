8ffd7524f0f0e69f6c7b4fc153ab8dc303d1ee8c
yangshuo <yangshuo@weidian.com>
2019-02-22 19:47:05 +0800

    ac

--- IdeaProjects/src/exam/medium/temp/SearchforaRange.java
+++ IdeaProjects/src/exam/medium/temp/SearchforaRange.java
@@ -8,18 +8,31 @@ import java.util.Arrays;
  * @comment
  */
 
-//34. Find First and Last Position of Element in Sorted Array
+/*
+34. Find First and Last Position of Element in Sorted Array
+ */
     //查找某个元素在数组中，第一次和最后一次出现的位置
     //数组是一个有序的
 public class SearchforaRange {
     //直接使用二分查找
     //找到相应元素后就向左右遍历
+    public static void main(String[] args) {
 
-    public int[] searchRange(int[] nums, int target) {
+
+        int[] a = new int[]{2,2};
+          int[] b=new int[2];
+          b=searchRange(a,2);
+        System.out.print("结果是" + b[0]+"-----"+b[1]);
+    }
+    public static int[] searchRange(int[] nums, int target) {
+        int[] a=new int[]{-1,-1};
+
+        if(nums.length<1 || nums ==null){
+            return a;
+        }
 
         int min=nums.length/2;
 
-        int[] a=new int[]{-1,-1};
         while(nums[min]!=target ){
             if(min==0 || min==nums.length-1){
                 return a;
@@ -30,37 +43,54 @@ public class SearchforaRange {
                 min=min+(nums.length-min)/2;
             }
         }
+
         int i=min-1,j=min+1;
 
 
 
             if(min==0){
                 a[0]=min;
-                while(nums[j]==target){
-                    j++;
-                }
-                a[1]=j-1;
-            }else if(min==nums.length-1){
-                a[1]=min;
-                while(nums[i]==target){
-                    i--;
-                }
-                a[1]=i+1;
-            }else {
-                while(i>=0 && j<nums.length){
-                    if(nums[i]==target){
-                        i--;
-                    }
-                    if(nums[j]==target){
+                if(j<nums.length){
+                    while(nums[j]==target){
                         j++;
                     }
+                    a[1]=j-1;
+                }else{
+                    a[1]=min;
+                }
+
+            }else if(min==nums.length-1){
+                a[1]=min;
+                if(i>=0){
+                    while(nums[i]==target){
+                        i--;
+                    }
+                    a[0]=i+1;
+                }else{
+                    a[0]=min;
+                }
+
+            }else {
+
+                while(i>=0){
+                    if(nums[i]==target){
+                        i--;
+                        continue;
+                    }
+                    break;
+                }
+                while(j<nums.length){
+                    if(nums[j]==target){
+                        j++;
+                        continue;
+
+                    }
+                    break;
                 }
                 a[0]=i+1;
                 a[1]=j-1;
 
             }
-
-
 
 
 
